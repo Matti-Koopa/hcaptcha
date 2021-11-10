7b7d3211d18689493562f6b60938be1d180a8183
Matthias Porges <matthias.porges@wien.gv.at>
2021-11-10 19:36:54 +0100

    Added Refit configuration param

--- src/HCaptcha.AspNetCore/HCaptchaExtensions.cs
+++ src/HCaptcha.AspNetCore/HCaptchaExtensions.cs
@@ -13,17 +13,29 @@ namespace BenjaminAbt.HCaptcha.AspNetCore
         /// </summary>
         public static IServiceCollection AddHCaptcha(this IServiceCollection services, IConfigurationSection section)
         {
+            return AddHCaptcha(services, section, null);
+        }
+
+        /// <summary>
+        /// Adds the HCaptcha configuration <see cref="section"/> as <see cref="HCaptchaOptions"/>.
+        /// Adds <see cref="IHCaptchaApi"/> as Refit Client with given base address in <see cref="HCaptchaOptions"/>.
+        /// </summary>
+        /// <param name="configureRefit">Custom Refit client configurations</param>
+        public static IServiceCollection AddHCaptcha(this IServiceCollection services, IConfigurationSection section,
+            Action<IHttpClientBuilder> configureRefit)
+        {
             HCaptchaOptions captchaOptions = new HCaptchaOptions();
             section.Bind(captchaOptions);
 
             services.Configure<HCaptchaOptions>(section);
 
-            services.AddRefitClient<IHCaptchaApi>()
+            var builder = services.AddRefitClient<IHCaptchaApi>()
                 .ConfigureHttpClient(c =>
                     {
                         c.BaseAddress = new Uri(captchaOptions.ApiBaseUrl);
                     }
                 );
+            configureRefit?.Invoke(builder);
 
             services.AddScoped<IHCaptchaProvider, HCaptchaProvider>();
 
